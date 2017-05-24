3b18d96542252b1b853d75fdcb660ad51c9e37ca
thanh1831995 <lethanh1831995@gmail.com>
2017-05-24 21:06:16 +0700

    Thanh+timKiem

--- quanlykho/quanlykho/Timkiem.Designer.cs
+++ quanlykho/quanlykho/Timkiem.Designer.cs
@@ -30,17 +30,19 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Timkiem));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txttim = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.txttim = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnTimKiem);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.txttim);
             this.groupBox1.Controls.Add(this.comboBox1);
@@ -50,6 +52,47 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Chọn Tiêu chí";
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(568, 31);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(116, 31);
+            this.btnTimKiem.TabIndex = 3;
+            this.btnTimKiem.Text = "Tìm Kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Search";
+            // 
+            // txttim
+            // 
+            this.txttim.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttim.Location = new System.Drawing.Point(287, 34);
+            this.txttim.Name = "txttim";
+            this.txttim.Size = new System.Drawing.Size(251, 22);
+            this.txttim.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "mahanghoa",
+            "TenHang"});
+            this.comboBox1.Location = new System.Drawing.Point(98, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(161, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = " ";
             // 
             // dataGridView1
             // 
@@ -62,38 +105,6 @@
             this.dataGridView1.Size = new System.Drawing.Size(873, 169);
             this.dataGridView1.TabIndex = 1;
             // 
-            // comboBox1
-            // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Mã hàng ",
-            "Tên hàng"});
-            this.comboBox1.Location = new System.Drawing.Point(98, 34);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(161, 24);
-            this.comboBox1.TabIndex = 0;
-            this.comboBox1.Text = "Mã hàng ";
-            // 
-            // txttim
-            // 
-            this.txttim.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txttim.Location = new System.Drawing.Point(287, 34);
-            this.txttim.Name = "txttim";
-            this.txttim.Size = new System.Drawing.Size(251, 22);
-            this.txttim.TabIndex = 1;
-            this.txttim.TextChanged += new System.EventHandler(this.txttim_changed);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(25, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 16);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Search";
-            // 
             // Timkiem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -104,7 +115,6 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "Timkiem";
             this.Text = "Timkiem";
-            this.Load += new System.EventHandler(this.Timkiem_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -120,5 +130,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txttim;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnTimKiem;
     }
 }
--- quanlykho/quanlykho/Timkiem.cs
+++ quanlykho/quanlykho/Timkiem.cs
@@ -7,29 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace quanlykho
 {
     public partial class Timkiem : Form
     {
+        SqlConnection con = new SqlConnection("data source=8470P-PC\\CNTTSQL;initial catalog=quanlykho;integrated security=True;MultipleActiveResultSets=True");
         public Timkiem()
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
+        void Hienthi()
         {
-
+            con.Open();
+            string str = string.Format("select hh.ma_hh as[mã hàng hóa],hh.ten_hh as [Tên hàng hóa],hh.ma_nhom as[Nhóm hàng hóa],hh.dvt as[dvt] from hanghoa hh");
+            SqlDataAdapter da = new SqlDataAdapter(str, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
         }
-
-        private void txttim_changed(object sender, EventArgs e)
+        private void btnTimKiem_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void Timkiem_Load(object sender, EventArgs e)
-        {
-
+            con.Open();
+            string str = string.Format("{0}_Timkiem", comboBox1.SelectedItem.ToString());
+            SqlCommand sc = new SqlCommand(str, con);
+            sc.CommandType = CommandType.StoredProcedure;
+            sc.Parameters.Add(new SqlParameter("@keyword", txttim.Text));
+            SqlDataAdapter da = new SqlDataAdapter(sc);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
         }
     }
 }
